a94cfe2a5b74766feeab4830f5b6ad4e55a1e9e6
Vegard Hansen Stenberg <vegard.koder@gmail.com>
2020-09-03 14:42:28 +0200

    socket added

--- initalRelease.py
+++ initalRelease.py
@@ -7,3 +7,9 @@ Created on Thu Sep  3 11:27:40 2020
 
 import socket
 
+inter = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+inter.connect(('192.168.1.147', 8080))
+    
+inter.sendall(b'10')
+inter.close()
