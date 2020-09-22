0edf1e9a9cb8f42c92c4c1ecc66c629fa1a8e586
SmileyFace799 <Henrikgran85@gmail.com>
2020-09-22 12:45:04 +0200

    Fixed if statements

--- marsRover.py
+++ marsRover.py
@@ -21,27 +21,20 @@ while True:
         data = connection.recv(4096)
         if not data: break
 
-<<<<<<< HEAD
-=======
 	direction = data.split()
 
-	if(direction[0] == 1)
-	{
+	if direction[0] == 1:
 		print("forward")
-	} elif(direction[1] == 1)
-	{
+
+	elif direction[1] == 1:
 		print("backwards")
-	}
 
-	if(direction[2] == 1)
-	{
+	if direction[2] == 1:
 		print("right")
-	} elif(direction[3] == 1)
-	{
+		
+	elif direction[3] == 1:
 		print("left")
-	}
 
->>>>>>> 16d136c9d3f871e6cdec90f490336aa0dabbc833
 	for key in enumerate(data):
 		print(key)
 		GPIO.output(outs[int(key[0])], int(key[1]))
