63c80417cb3ddc234ce0b6e3a3fb65e73478d3fc
villeviking <villeviking9000@gmail.com>
2020-10-14 14:03:44 +0200

    high ress ui

--- initialRelease.py
+++ initialRelease.py
@@ -21,11 +21,11 @@ else: joystick.init()
 
 axis_input = [0, 0]
 while True:
+    c.tick(30)
     for event in pg.event.get():
         if event.type == pg.QUIT: exit()
         elif joystick and event.type == pg.JOYAXISMOTION:
             axis_input[event.axis] = event.value if abs(event.value) > 0.2 else 0
-    c.tick(30)
     key_in = pg.key.get_pressed()
     light_sequence = ['0', '0', '0', '0']
     if joystick:
@@ -40,8 +40,33 @@ while True:
     else:
         for i in enumerate([pg.K_w, pg.K_a, pg.K_s, pg.K_d]):
             light_sequence[i[0]] = str(key_in[i[1]])
+
+    w = pg.draw.rect(screen, (255, 255, 255), (0, 150, 100, 100))
+    a = pg.draw.rect(screen, (255, 255, 255), (110, 40, 100, 100))
+    s = pg.draw.rect(screen, (255, 255, 255), (110, 150, 100, 100))
+    d = pg.draw.rect(screen, (255, 255, 255), (220, 150, 100, 100))
+    if pg.mouse.get_pressed(1)[0]:
+        if w.collidepoint(pg.mouse.get_pos()):
+            light_sequence[1] = '1'
+        if a.collidepoint(pg.mouse.get_pos()):
+            light_sequence[0] = '1'
+        if s.collidepoint(pg.mouse.get_pos()):
+            light_sequence[2] = '1'
+        if d.collidepoint(pg.mouse.get_pos()):
+            light_sequence[3] = '1'
+
     light_string = ''.join(light_sequence).encode('utf-8')
     inter.sendall(light_string)
+
+    if int(light_sequence[0]):
+        pg.draw.rect(screen, (255, 0, 0), (120, 50, 80, 80))
+    if int(light_sequence[1]):
+        pg.draw.rect(screen, (255, 0, 0), (10, 160, 80, 80))
+    if int(light_sequence[2]):
+        pg.draw.rect(screen, (255, 0, 0), (120, 160, 80, 80))
+    if int(light_sequence[3]):
+        pg.draw.rect(screen, (255, 0, 0), (230, 160, 80, 80))
     pg.display.flip()
+    screen.fill((0, 0, 0))
 
 inter.close()
