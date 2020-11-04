1847a2a4f46339ffad818d6cb4f2a93c09b16cb8
SmileyFace799 <Henrikgran85@gmail.com>
2020-11-04 13:34:28 +0100

    Fiksa nån constants, har også error handling hvis det oppstår ein feil

--- constants.py
+++ constants.py
@@ -1,3 +1,9 @@
-#Shared
+from pygame import freetype as ft
+ft.init()
+
 pi_ip = '192.168.1.59'
 pg_screen_res = (1280, 720)
+rgb_white = (255, 255, 255)
+rgb_red = (255, 0, 0)
+
+font_arial = ft.SysFont('Arial', 70)
