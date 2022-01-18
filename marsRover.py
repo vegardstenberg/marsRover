36f7974141860e49c203c10812e654a49e31bcbc
SmileyFace799 <Henrikgran85@gmail.com>
2022-01-18 14:02:58 +0100

    uhh

--- marsRover.py
+++ marsRover.py
@@ -24,7 +24,7 @@ class Events:
 
 	class ReverseEvent(ActionEvent):
 		def run(self, **kwargs):
-			reverse(kwargs['speed']) #h
+			reverse(kwargs['speed'])
 
 	class TurnLeftEvent(ActionEvent):
 		def run(self, **kwargs):
