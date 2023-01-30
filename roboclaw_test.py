109b1ee8bae1e5ed15d2b6df0a5b26e01ed4d7fb
ic1cle <sigsos1@hotmail.com>
2023-01-30 09:35:30 +0100

    dasad

--- roboclaw_test.py
+++ roboclaw_test.py
@@ -6,4 +6,9 @@ roboclaw.Open()
 
 if __name__ == "__main__":
 	while True:
-		roboclaw.ForwardM1(address, 64)
+		roboclaw.ForwardM1(address[1], 64)
+		roboclaw.ForwardM2(address[1], -64)
+		roboclaw.ForwardM1(address[2], 64)
+		roboclaw.ForwardM2(address[2], -64)
+		roboclaw.ForwardM1(address[3], 64)
+		roboclaw.ForwardM2(address[3], -64)
