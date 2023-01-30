c6d49089b00c24e66d6b021914705bdf9e356cbe
ic1cle <sigsos1@hotmail.com>
2023-01-30 09:44:23 +0100

    removed loop

--- roboclaw_test.py
+++ roboclaw_test.py
@@ -12,11 +12,10 @@ roboclaw = Roboclaw("/dev/ttyS0", 38400)
 roboclaw.Open()
 
 if __name__ == "__main__":
-	while True:
-		roboclaw.ForwardM1(address[1], 64)
-		roboclaw.ForwardM2(address[1], 64)
-		roboclaw.ForwardM1(address[2], 64)
-		roboclaw.ForwardM2(address[2], 64)
-		roboclaw.ForwardM1(address[3], 64)
-		roboclaw.ForwardM2(address[3], 64)
+	roboclaw.ForwardM1(address[1], 64)
+	roboclaw.ForwardM2(address[1], 64)
+	roboclaw.ForwardM1(address[2], 64)
+	roboclaw.ForwardM2(address[2], 64)
+	roboclaw.ForwardM1(address[3], 64)
+	roboclaw.ForwardM2(address[3], 64)
 
