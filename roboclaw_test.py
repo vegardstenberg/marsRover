796e123378eb2fe476f370fa303c95ecdabce37d
ic1cle <sigsos1@hotmail.com>
2023-01-30 09:42:31 +0100

    roboclaw:test

--- roboclaw_test.py
+++ roboclaw_test.py
@@ -1,14 +1,22 @@
 from roboclaw import Roboclaw
 
-address = 0x80
+address = {
+		1: 0x80, #front motors
+		2: 0x81, #mid motors
+		3: 0x82, #back motors
+		4: 0x83, #front steering
+		5: 0x84 #back steering
+	}
+
 roboclaw = Roboclaw("/dev/ttyS0", 38400)
 roboclaw.Open()
 
 if __name__ == "__main__":
 	while True:
 		roboclaw.ForwardM1(address[1], 64)
-		roboclaw.ForwardM2(address[1], -64)
+		roboclaw.ForwardM2(address[1], 64)
 		roboclaw.ForwardM1(address[2], 64)
-		roboclaw.ForwardM2(address[2], -64)
+		roboclaw.ForwardM2(address[2], 64)
 		roboclaw.ForwardM1(address[3], 64)
-		roboclaw.ForwardM2(address[3], -64)
+		roboclaw.ForwardM2(address[3], 64)
+
