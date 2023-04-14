fdac7e8ce0a7c0fadebfab4d9c63d6777ff6f276
ic1cle <sigsos1@hotmail.com>
2023-04-14 10:42:28 +0200

    s

--- marsRover.py
+++ marsRover.py
@@ -253,12 +253,12 @@ if __name__ == '__main__':
 		setup()
 		print('Setup completed with default ip')
 	except:
-		retry_query = input('Setup failed. Do you want to...\n  1. Retry with different ip\n  2. Run local testing\n 3. Kill Processes  4. Exit\nResponse: ')
+		retry_query = input('Setup failed. Do you want to...\n  1. Retry with different ip\n  2. Run local testing\n 3. Kill Processes\n  4. Exit\nResponse: ')
 		while True:
 			if retry_query.isdigit() and 1 <= int(retry_query) <= 4:
 				retry_query = int(retry_query)
 				break
-			retry_query = input('Invalid response. Do you want to...\n  1. Retry with different ip\n  2. Run local testing\n 3. Kill Processes  4. Exit\nResponse: ')
+			retry_query = input('Invalid response. Do you want to...\n  1. Retry with different ip\n  2. Run local testing\n 3. Kill Processes\n  4. Exit\nResponse: ')
 		if retry_query == 1:
 			new_ip = input('Enter new ip address: ')
 			setup(ip=new_ip)
